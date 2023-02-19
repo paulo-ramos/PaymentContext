35a0e9eb4af6901e769aa4e7306649d89489c98f
paulo-ramos <paulo_ramos@live.com>
2023-02-19 10:32:25 -0300

    Add project files.

---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Subscription
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public bool Active { get; set; }
+        public List<Payment>? Payments { get; set; }
+    }
+}
